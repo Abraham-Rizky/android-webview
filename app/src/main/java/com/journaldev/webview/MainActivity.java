cf81357a7e69cc3d8c6066a365bd726965724069
Abraham-Rizky <35181977+Abraham-Rizky@users.noreply.github.com>
2019-07-22 19:43:23 -0700

    Add android files

---
+++ app/src/main/java/com/journaldev/webview/MainActivity.java
@@ -0,0 +1,47 @@
+package com.journaldev.webview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Iterator;
+
+public class MainActivity extends Activity {
+    private WebView webView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.webView = (WebView) findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        WebViewClientImpl webViewClient = new WebViewClientImpl(this);
+        webView.setWebViewClient(webViewClient);
+        webView.loadUrl("http://192.168.43.1:8080/MVCApplication/");
+        //webView.loadData("<html><body>Hello, world!</body></html>", "text/html", "UTF-8");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.webView.canGoBack()) {
+            this.webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
